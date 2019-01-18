5f8be3bd464b0353ef54eb002ff57c6403a256cf
ForeManWang <formanW@aliyun.com>
2019-01-18 15:50:15 +0800

    事件

--- event/pages/index/index.js
+++ event/pages/index/index.js
@@ -11,7 +11,6 @@ Page({
     console.log('outter')
   },
   tap2Handle: function (e) {
-    // e.target 拿到的就是点击的元素
     // dataset指的是元素上所有以data-开头的属性集合
     console.dir(e.target.dataset) 
     // console.log(this) // 事件处理函数中的this指向的还是页面对象！！！跟跟HTML不一样
