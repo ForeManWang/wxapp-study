6a1a9566a07094acd9f7ee62a3682c4d5d97e1f4
ForeManWang <formanW@aliyun.com>
2019-01-18 15:49:32 +0800

    setData

--- data-flow/pages/index/index.js
+++ data-flow/pages/index/index.js
@@ -10,9 +10,5 @@ Page({
             message: e.detail.value
         })
         console.log(this.data)
-
-        //  this.setData 是用来改变data中的数据 
-        // 他与直接赋值区别在于setData可以通知界面做出变化
-        // 直接赋值没有办法实现这一点（早期的的JS）
     }
 })
